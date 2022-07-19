9826219e455c805476ddb185fcc092cd661bd3b3
zhangjian <zhangjian_9253@163.com>
2022-07-19 11:28:30 +0800

    feat:

--- dailyJs/deepClone.js
+++ dailyJs/deepClone.js
@@ -20,26 +20,51 @@
 
 // 全面的深克隆
 
-function getType(obj) {
-    return Object.prototype.toString.call(obj).replace(/\[｜\]|\object /g, '')
-}
-function fullClone (target) {
-    const type = getType(target);
-    let cloneObj = {};
+
+function fullClone (target, wm = new WeakMap()) {
+    let res = null;
     const reference = ["Set", "WeakSet", "Map", "WeakMap", "RegExp", "Date", "Error"];
-    if (type === 'object') {
-        for(let key in target) {
-            cloneObj[key] = fullClone(target[key])
-        }
-    } else if (type === 'Array') {
-        cloneObj = [];
-        target.forEach((item) => {
-            cloneObj.push(fullClone(item)) 
-        })
-    } else if (reference.includes(type)) {
-        cloneObj = new target.constructor(target)
+    if (reference.includes(target.constructor)) {
+        res = new target.constructor(target)
+    } else if (typeof target === 'Object' && type !== null) {
+       if (wm.has(target)) {
+           return wm.get(target)
+       }
+       const allDescs = Object.getOwnPropertyDescriptors(target);
+       const cloneObj = Object.create(Object.getPrototypeOf(target), allDescs);
+       wm.set(target, cloneObj);
+       console.log(target)
+       for (let key in Reflect.ownKeys(target)) {
+        cloneObj[key] = fullClone(target[key], wm);
+       }
+       res = cloneObj
     } else {
-        cloneObj = target
+        res = target
+    }
+    return res
+}
+function deepCopy (target, wm = new WeakMap()) {
+    const isComplexDataType = (o) => (typeof o === 'object' || typeof o === 'function') && o !== null
+    if (target.constructor === Date) {
+        return new Date(target)
+    }
+    if (target.constructor === RegExp) {
+        return new RegExp(target)
+    }
+    //如果循环引用了就用 weakMap 来解决
+    if (wm.has(target)) {
+        return wm.get(target)
+    }
+    // 获取对象的所有属性
+    let allDesc = Object.getOwnPropertyDescriptors(target)
+    //遍历传入参数所有键的特性
+    let cloneObj = Object.create(Object.getPrototypeOf(target), allDesc)
+    // 把对象里面所有属性缓存起来
+    wm.set(target, cloneObj)
+    // 这里解释一些为什么不用for (let key in target).
+    // 因为不能把不可枚举属性带出来。 使用Reflect.ownKeys(target)可以把不可枚举属性也列出来
+    for (let key of Reflect.ownKeys(target)) {
+        cloneObj[key] = (isComplexDataType(target[key]) && typeof target[key] !== 'function') ? deepCopy(target[key], wm) : target[key]
     }
     return cloneObj
 }
@@ -75,9 +100,13 @@ const target = {
     },
     func2: function (a, b) {
         return a + b;
-    },
+    }
 };
 //测试代码
 const test1 = fullClone(target);
 target.field4.push(9);
+target.loop = target;
 console.log('test1: ', test1);
+
+
+// 带上解决循环引用的深克隆
